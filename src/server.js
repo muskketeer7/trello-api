0ea3f09b41f6e83b0f5c26ba807218076900093e
vinh-hoang-ngoc <vinhhoangngoc22@gmail.com>
2024-03-26 23:23:29 +0700

    validation title and description

--- src/server.js
+++ src/server.js
@@ -8,6 +8,9 @@ import { CLOSE_DB, CONNECT_DB } from './config/mongodb'
 
 const START_SERVER = () => {
   const app = express()
+
+  app.use(express.json())
+
   app.use('/v1', APIs_V1)
 
   app.listen(env.APP_PORT, env.APP_HOST, () => {
---
+++ src/validations/boardValidations.js
@@ -0,0 +1,47 @@
+import { StatusCodes } from 'http-status-codes'
+import Joi from 'joi'
+
+const createNew = async (req, res, next) => {
+  const correctCondition = Joi.object({
+    title: Joi.string().required().min(3).max(50).trim().strict().messages({
+      'any.required': 'Title is required',
+      'string.empty': 'Title is not allowed to be empty',
+      'string.min': 'Title length must be at least 3 chars',
+      'string.max': 'Title maximum must be at least 50 chars',
+      'string.trim': 'Title must not hvae leading or trailing whitespace'
+    }),
+    description: Joi.string()
+      .required()
+      .min(3)
+      .max(200)
+      .trim()
+      .strict()
+      .messages({
+        'any.required': 'Description is required',
+        'string.empty': 'Description is not allowed to be empty',
+        'string.min': 'Description length must be at least 3 chars',
+        'string.max': 'Description maximum must be at least 50 chars',
+        'string.trim':
+          'Description must not hvae leading or trailing whitespace'
+      })
+  })
+
+  try {
+    console.log(req.body)
+
+    await correctCondition.validateAsync(req.body, { abortEarly: false })
+
+    res
+      .status(StatusCodes.CREATED)
+      .json({ message: 'GET: API get list boards' })
+  } catch (error) {
+    console.log(error)
+    res
+      .status(StatusCodes.UNPROCESSABLE_ENTITY)
+      .json({ errors: new Error(error).message })
+  }
+}
+
+export const boardValidation = {
+  createNew
+}
